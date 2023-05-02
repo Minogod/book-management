2fe083d3a199353dbf0f90a9ec6d8f06eb3fd7a3
MINHO <93550624+Minogod@users.noreply.github.com>
2023-05-02 13:32:35 +0900

    Initial commit

---
+++ src/test/java/server/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package server.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
